e8bcdf99400b3326e9d168e7d28d07002de40b6b
Brais Calvo <bcalvo@SIGA98.local>
2017-08-25 14:59:53 +0200

    changes

--- src/components/dashboard/navbar/Navbar.jsx
+++ src/components/dashboard/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import uuid from 'uuid'
 const Navbar = (nav)=>(<div>
     {
         nav.map( (item)=>{
-            
+                <NavbarItem key={uuid.v4()} {...item} onDisplayedItem={onDisplayedItem} />
         }
 
         )
