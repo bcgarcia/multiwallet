38d29de29a3c354c6ab2ed018105d371aef97af3
Brais Calvo <bcalvo@SIGA98.local>
2017-09-28 18:59:19 +0200

    added new actions

--- src/actions/appActions.js
+++ src/actions/appActions.js
@@ -1,7 +1,9 @@
-import { TOGGLE_HEADER,TOGGLE_USER_NOTIFICATIONS,TOGGLE_USER_OPTIONS } from '../constants/actions'
+import { TOGGLE_HEADER, TOGGLE_USER_NOTIFICATIONS, TOGGLE_USER_OPTIONS, TOGGLE_SIDEBAR } from '../constants/actions'
 
-export function toggleHeader(state) {return {type: TOGGLE_HEADER,payload:state}}
+export function toggleHeader(state) {return { type: TOGGLE_HEADER , payload:state } }
 
-export function toggleUserNotifications(state){ return { type: TOGGLE_USER_NOTIFICATIONS,payload : state } }
+export function toggleUserNotifications(state){ return { type: TOGGLE_USER_NOTIFICATIONS , payload : state } }
 
-export function toggleUserOptions(state){ return{ type: TOGGLE_USER_OPTIONS,payload : state } }
+export function toggleUserOptions(state){ return{ type: TOGGLE_USER_OPTIONS , payload : state } }
+
+export function toggleSidebar(state){ return {type: TOGGLE_SIDEBAR , payload: state } }
